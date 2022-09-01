8c46eed89c7e24a640ebcdc84f233aa8738dafce
Noah Gilson <noahgilson@microsoft.com>
2022-09-01 11:15:56 -0700

    Re-enable test for rid agnostic libraries since daniels msbuild patch has merged in

--- src/Cli/Microsoft.TemplateEngine.Cli/Commands/HelpStrings.Designer.cs
+++ src/Cli/Microsoft.TemplateEngine.Cli/Commands/HelpStrings.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace Microsoft.TemplateEngine.Cli.Commands {
     using System;
+    using System.Reflection;
     
     
     /// <summary>
@@ -39,7 +40,7 @@ namespace Microsoft.TemplateEngine.Cli.Commands {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.TemplateEngine.Cli.Commands.HelpStrings", typeof(HelpStrings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.TemplateEngine.Cli.Commands.HelpStrings", typeof(HelpStrings).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
