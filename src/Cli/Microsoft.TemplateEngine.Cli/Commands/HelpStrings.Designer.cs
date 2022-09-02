9a5405d92473a292fa4eaecbfc29b775c25896b8
Noah Gilson <noahgilson@microsoft.com>
2022-09-02 10:09:18 -0700

    Improve error message

--- src/Cli/Microsoft.TemplateEngine.Cli/Commands/HelpStrings.Designer.cs
+++ src/Cli/Microsoft.TemplateEngine.Cli/Commands/HelpStrings.Designer.cs
@@ -10,7 +10,6 @@
 
 namespace Microsoft.TemplateEngine.Cli.Commands {
     using System;
-    using System.Reflection;
     
     
     /// <summary>
@@ -40,7 +39,7 @@ namespace Microsoft.TemplateEngine.Cli.Commands {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.TemplateEngine.Cli.Commands.HelpStrings", typeof(HelpStrings).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.TemplateEngine.Cli.Commands.HelpStrings", typeof(HelpStrings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
